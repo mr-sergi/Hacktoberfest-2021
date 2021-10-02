a564d14a668072910c42a1f4a189d1137dddb9e4
Ranu311 <71586938+Ranu311@users.noreply.github.com>
2021-10-02 15:05:24 +0530

    Update main.java

--- main.java
+++ main.java
@@ -6,6 +6,7 @@ public class Main {
     for (int i = 1; i <= (last - 'A' + 1); ++i) {
       for (int j = 1; j <= i; ++j) {
         System.out.print(alphabet + " ");
+        System.out.print(avh);
       }
       ++alphabet;
 
