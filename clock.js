0e250b2b3bd10686c6a371d55d168464fbbcaa1f
Ranu311 <71586938+Ranu311@users.noreply.github.com>
2021-10-02 15:02:28 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -68,7 +68,7 @@
     </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <h1>hello</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
