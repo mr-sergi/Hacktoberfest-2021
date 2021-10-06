bb1a7886d200e2e88a3a26660ef1632a7a26a392
Abhishek Sharma <58878438+imgeekabhi@users.noreply.github.com>
2021-10-06 18:07:11 +0530

    Update Prime.c
    Less line of code and easy to understand

--- Prime.c
+++ Prime.c
@@ -1,19 +1,16 @@
-#include<stdio.h>  
-int main(){    
-int n,i,m=0,flag=0;    
-printf("Enter the number to check prime:");    
-scanf("%d",&n);    
-m=n/2;    
-for(i=2;i<=m;i++)    
-{    
-if(n%i==0)    
-{    
-printf("Number is not prime");    
-flag=1;    
-break;    
-}    
-}    
-if(flag==0)    
-printf("Number is prime");     
-return 0;  
- }    
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int n,temp=0;
+    printf("Enter the number to check prime:");
+    scanf("%d",&n);
+    for(int i=2 ; i<n ; i++){
+        if(n%i==0)
+            temp++;
+    }
+    if(temp>0)
+        printf("%d is not a Prime Number",n);
+    else
+        printf("%d is a Prime Number",n);
+    return 0;
+ }
