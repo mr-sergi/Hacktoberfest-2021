bc999fa2c8c025b0ab1626bff2eb13b5c5e4ea0b
Aliza Dixit <alizadixit6@gmail.com>
2021-10-16 14:22:43 +0530

    program

--- Kadane.java
+++ Kadane.java
@@ -1,22 +1,29 @@
-class KadaneAlgo{
-    // arr: input array
-    // n: size of array
-    //Function to find the sum of contiguous subarray with maximum sum.
-    long maxSubarraySum(int arr[], int n){
-        long currSum=0; //track current sum
-        long sumSoFar=Long.MIN_VALUE;
-        
-        for(int i=0;i<n;i++){
-            currSum+=arr[i];
-            
-            if(currSum<arr[i]) currSum=arr[i]; //check current sum
-            
-            if(sumSoFar<currSum) sumSoFar=currSum; //update max sum
-        }
-        return sumSoFar;
-        
-    }
-//     Time Complexity: O(N)
-//     Auxiliary Space: O(1)
-}
+import java.io.*;
 
+import java.util.*;
+ 
+class Kadane
+{
+    public static void main (String[] args)
+    {
+        int [] a = {-1, -6, 9, 0, -1, 1, 4, -5};
+        System.out.println("Maximum contiguous sum is " +
+                                       maxSubArraySum(a));
+    }
+ 
+    private static int maxSubArraySum(int a[])
+    {
+        int size = a.length;
+        int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+ 
+        for (int i = 0; i < size; i++)
+        {
+            max_ending_here = max_ending_here + a[i];
+            if (max_so_far < max_ending_here)
+                max_so_far = max_ending_here;
+            if (max_ending_here < 0)
+                max_ending_here = 0;
+        }
+        return max_so_far;
+    }
+}
