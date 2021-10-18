591972eaef304eaf6ddebf24e46d741eabdee7fe
SrushtiBankar <88138554+SrushtiBankar@users.noreply.github.com>
2021-10-18 20:24:25 +0530

    Create Addition.c

---
+++ Addition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int no1,no2,add;
+	
+	printf("Enter any two nos: ");
+	scanf("%d%d",&no1,&no2);
+	
+	add=no1+no2;
+	printf("\nNumber1\t\t : %d",no1);
+	printf("\nNumber2\t\t : %d",no2);
+	printf("\nAddition of two nos is\t : %d",add);
+	
+	return 0;
+	
+	
+}
+
